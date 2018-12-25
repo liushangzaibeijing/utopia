ac10b402d74e2195396374fc16024395baddf9ce
xieqx <1374523006@qq.com>
2018-12-26 00:07:33 +0800

    dubbo服务的验证

---
+++ CommonServiceProvider/src/test/java/com/xiu/followdouban/commonwebapi/lucene/LuceneDemoTest.java
@@ -0,0 +1,101 @@
+package com.xiu.followdouban.commonwebapi.lucene;
+
+import com.xiu.followdouban.commonrpc.model.Book;
+import com.xiu.followdouban.commonrpc.service.LuceneService;
+import com.xiu.followdouban.commonwebapi.CommonServiceProviderApplication;
+import com.xiu.followdouban.commonwebapi.utils.JsonUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * author   xieqx
+ * createTime  2018/12/2
+ * desc lucene
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CommonServiceProviderApplication.class)
+@WebAppConfiguration
+public class LuceneDemoTest {
+
+    @Autowired
+    private LuceneService luceneService;
+
+    //创建索引 书籍
+    @Test
+    public void  bookIndex(){
+         boolean flag =  luceneService.bookIndexInit("bookIndex");
+
+         if(flag){
+             log.info("更新索引成功");
+         }else{
+             log.info("更新索引失败");
+         }
+    }
+
+
+    //创建索引 书籍
+    @Test
+    public void  bookSearch(){
+        String[] fields = {"name","author"};
+        Map<String,Float> boots = new HashMap<>(2);
+        boots.put("name",1.0F);
+        boots.put("author",1.0F);
+        List<Book> books = null;
+        books = luceneService.bookSearch("bookIndex","未来简史",fields,boots,1);
+
+        log.info("查询结果 ： {}", JsonUtil.gsonString(books));
+    }
+
+    /**
+     * 为所有书籍新创建索引 Document中只包含id,name,author
+     */
+    @Test
+    public void StringIndex() {
+
+        IndexWriter writer = null;
+        try {
+            //文件索引存放路径
+            Directory directory = FSDirectory.open(Paths.get("bookIndex"));
+            //分词器
+            IKAnalyzer analyzer = new IKAnalyzer();
+            //StandardAnalyzer analyzer = new StandardAnalyzer();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            writer = new IndexWriter(directory, config);
+
+            //在第一次创建索引的时候
+            Document document = new Document();
+            document.add(new StringField("id","i am  student", Field.Store.YES));
+            writer.addDocument(document);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                writer.commit();
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
