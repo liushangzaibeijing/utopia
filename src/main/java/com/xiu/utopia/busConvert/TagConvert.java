aad66a4287598b935a52ccb7fa76aeaad6666aa8
liushangzaibeijing <1374523006@qq.com>
2021-02-03 14:46:40 +0800

    修改qq登录的json转换

--- src/main/java/com/xiu/utopia/busConvert/TagConvert.java
+++ src/main/java/com/xiu/utopia/busConvert/TagConvert.java
@@ -16,7 +16,6 @@ import com.xiu.utopia.vo.MovieVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -168,6 +167,7 @@ public class TagConvert{
             }
         }
     }
+
 
 
 
--- src/test/java/com/xiu/utopia/test/BuninessServiceTest.java
+++ src/test/java/com/xiu/utopia/test/BuninessServiceTest.java
@@ -2,6 +2,9 @@ package com.xiu.utopia.test;
 
 import com.xiu.utopia.UtopiaApplication;
 import com.xiu.utopia.busConvert.TagConvert;
+import com.xiu.utopia.dao.BookMapper;
+import com.xiu.utopia.entity.Book;
+import com.xiu.utopia.entity.BookExample;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * author  Administrator
@@ -22,6 +29,9 @@ public class BuninessServiceTest {
 
     @Autowired
     TagConvert tagConvert;
+
+    @Autowired
+    BookMapper bookMapper;
 
     @Test
     public void test(){
@@ -44,6 +54,36 @@ public class BuninessServiceTest {
     }
 
 
+    @Test
+    public void updateBookBatch() {
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(100,100,100, TimeUnit.MICROSECONDS,
+                new ArrayBlockingQueue<Runnable>(10));
+        for(int i = 0;i < 100; i++){
+            executor.execute(new UpdateTask(bookMapper));
+        }
+    }
+
+
+
+    class UpdateTask implements Runnable{
+        private BookMapper bookMapper;
+
+
+        public UpdateTask(BookMapper bookMapper) {
+            this.bookMapper = bookMapper;
+        }
+
+        @Override
+        public void run() {
+            BookExample example = new BookExample();
+            example.createCriteria().andIdEqualTo(2439);
+            Book book = new Book();
+            book.setScore(0.9);
+            int i = bookMapper.updateByExampleSelective(book, example);
+            log.info("i:{}",i);
+        }
+    }
 
 
 
